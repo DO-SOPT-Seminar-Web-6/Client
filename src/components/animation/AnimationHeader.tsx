fd5f2bc391582cd7b1b8cffbfd9c5a7a56171a01
HAE2NI <hae0_02ni@ewhain.net>
2023-11-22 19:54:07 +0900

    [ design ] 애니메이션 썸네일(가장 위쪽 img)

---
+++ src/components/animation/AnimationHeader.tsx
@@ -0,0 +1,4 @@
+import collectDetailThumnailImg from '@assets/image/collection/collectDetail/collectDetailThumnailImg.png';
+export default function AnimationHeader() {
+  return <img src={collectDetailThumnailImg} alt="썸네일" />;
+}
