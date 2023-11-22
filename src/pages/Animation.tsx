d6c2ea6f0d250a172547adad6e04cbeaf2928c04
HAE2NI <hae0_02ni@ewhain.net>
2023-11-22 19:55:25 +0900

    [ design ] AnimationPage 추가

---
+++ src/pages/Animation.tsx
@@ -0,0 +1,12 @@
+import AnimationHeader from '@components/animation/AnimationHeader';
+import AnimationTitle from '@components/animation/AnimationTitle';
+import React from 'react';
+
+export default function Animation() {
+  return (
+    <>
+      <AnimationHeader />
+      <AnimationTitle />
+    </>
+  );
+}
