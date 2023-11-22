a6a958e8ac8958619a1afe802adafb7f0228b96d
Jeon Eonseok <jseokeon@gmail.com>
2023-11-22 21:40:02 +0900

    [ design ] 댓글 공통 컴포넌트 UI 생성

--- src/components/common/Comment/Comment.tsx
+++ src/components/common/Comment/Comment.tsx
@@ -1,50 +1,48 @@
-import ReviewStarFullIc from '@assets/icon/review/reviewStarFullIc.svg';
-import ReviewLikeCommentIc from '@assets/icon/review/reviewLikeCommentIc.svg';
-import ReviewLikeUnActiveIc from '@assets/icon/review/reviewLikeUnActiveIc.svg';
+import styled from 'styled-components';
+import CommentTop from './CommentTop';
+import CommentBottom from './CommentBottom';
 
-interface InCommentTypeProps {
+interface InContainerTypes {
+  isThumbnail?: boolean;
+}
+
+interface InCommentPropsTypes extends InContainerTypes {
   userName: string;
+  content: string;
 }
 
 /** Comment */
-export default function Comment(props: InCommentTypeProps) {
-  const { userName } = props;
+export default function Comment(props: InCommentPropsTypes) {
+  const { userName, content, isThumbnail = false } = props;
 
   return (
-    <article>
-      <div>
-        <img src="" alt="user-profile" />
-        <span>{userName}</span>
-        <div>
-          <img src={ReviewStarFullIc} />
-          <img src={ReviewStarFullIc} />
-          <img src={ReviewStarFullIc} />
-          <img src={ReviewStarFullIc} />
-          <img src={ReviewStarFullIc} />
-        </div>
-      </div>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio tempora reprehenderit aperiam ut nesciunt
-          perspiciatis, tenetur tempore unde ducimus dolore, cupiditate sapiente possimus libero placeat quibusdam saepe
-          officiis iste ab perferendis. Fuga voluptatem aliquid nesciunt commodi accusamus numquam deserunt totam
-          quaerat placeat qui, esse ullam quisquam eligendi molestias delectus perspiciatis eum veritatis dolorem
-          perferendis culpa voluptatum? Ipsam alias aliquid cum eaque. Aliquid molestias praesentium consequuntur eaque
-          pariatur exercitationem sequi numquam dicta ab, iure inventore quisquam, architecto, perferendis placeat quasi
-          fugit corporis cupiditate ut? Rerum laborum aut dolorem iste tempore ad iusto corrupti? Veniam voluptatum
-          accusantium, suscipit aut eligendi maiores reprehenderit?
-        </p>
-      </div>
-      <div>
-        <button>
-          <img src={ReviewLikeUnActiveIc} />
-          <span>755</span>
-        </button>
-        <button>
-          <img src={ReviewLikeCommentIc} />
-          <span>14</span>
-        </button>
-      </div>
-    </article>
+    <CommentContainer isThumbnail={isThumbnail}>
+      <CommentTop userName={userName} isThumbnail={isThumbnail} />
+      <CommentMiddleText>{content}</CommentMiddleText>
+      <CommentBottom />
+    </CommentContainer>
   );
 }
+
+const CommentContainer = styled.article<InContainerTypes>`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ isThumbnail }) => (isThumbnail ? '1.2rem' : '1.4rem')};
+  border-radius: 6px;
+  background-color: ${({ theme }) => theme.colors.grey100};
+  padding: 1.2rem;
+  width: ${({ isThumbnail }) => (isThumbnail ? '30.4rem' : '48.9rem')};
+  height: ${({ isThumbnail }) => (isThumbnail ? '25rem' : '25.1rem')};
+`;
+
+const CommentMiddleText = styled.p<InContainerTypes>`
+  ${({ theme }) => theme.fonts.body2160};
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+  display: -webkit-box; /* stylelint-disable-line value-no-vendor-prefix */
+  width: 100%;
+  height: ${({ isThumbnail }) => (isThumbnail ? '12.2rem' : '12.1rem')};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  line-height: 2.3rem;
+`;
