160292adb6c249fd947f151c6f2334a65380cf1c
Rose <120544840+Rose-my@users.noreply.github.com>
2023-11-23 04:50:55 +0900

    [ design ] 아래에 버튼 공통컴포넌트 추가

--- src/components/similar/Similar.tsx
+++ src/components/similar/Similar.tsx
@@ -1,22 +1,31 @@
 import SimilarHeader from "./SimilarHeader";
-import { SectionContainer } from "@styles/globalStyle";
+import { SectionContainer , flexCenter} from "@styles/globalStyle";
 import SimilarArticle from "./SimilarArticle";
 import styled from "styled-components";
+import Button from "@components/common/Button/Button";
 
 export default function Similar() {
 
   return (
-    <Section>
-      <SimilarHeader/>
-      <SimilarArticle/>
-    </Section>
+    <Container>
+      <Section>
+        <SimilarHeader/>
+        <SimilarArticle/>
+      </Section>
+      <Button/>
+    </Container>
   )
 }
 
-const Section = styled(SectionContainer)`
+const Container = styled(SectionContainer)`
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  padding: 15.5rem 5.9rem 10rem 5.9rem;
+`
+const Section = styled.section`
   display: flex;
   flex-direction:column;
   gap: 3.5rem;
-  padding: 15.5rem 5.9rem 5.5rem 5.9rem;
-  height: 70.8rem
+  height: 50.8rem
 `;
