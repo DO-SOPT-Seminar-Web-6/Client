74eccff20c385ca0103b041005dd9415d52cf6c0
Jeon Eonseok <jseokeon@gmail.com>
2023-11-23 16:42:29 +0900

    [ fix ] 컴포넌트 이름 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
+
+import {RouterProvider} from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import Header from '@components/layout/Header/Header';
-import Footer from '@components/layout/Footer/Footer';
 import GlobalStyle from '@styles/globalStyle';
 import theme from '@styles/theme';
-import Animation from '@pages/Animation';
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <RouterProvider router={router}/>
       <GlobalStyle />
-      <Header />
-      <main>
-        <Animation />
-      </main>
-      <Footer />
     </ThemeProvider>
   );
 }
