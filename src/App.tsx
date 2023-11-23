68ea51b1cbdd187df7779a86c03b304828aee17c
Rose <120544840+Rose-my@users.noreply.github.com>
2023-11-23 19:29:26 +0900

    [ etc ] 불필요한 import 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import Header from '@components/layout/Header/Header';
 import Footer from '@components/layout/Footer/Footer';
 import GlobalStyle from '@styles/globalStyle';
 import theme from '@styles/theme';
-import Animation from '@pages/Animation';
-import { BannerProfileIc } from './assets';
 import Main from '@pages/Main'; 
 
 function App() {
