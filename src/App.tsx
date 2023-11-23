ddc7df5c43ab7199bc867c1a999d47b340525d7b
Jeon Eonseok <jseokeon@gmail.com>
2023-11-23 16:41:13 +0900

    [ feat ] main layout 추가
    #22

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import MainLayout from '@components/layout/MainLayout';
+import GlobalStyle from '@styles/globalStyle';
+import theme from '@styles/theme';
 import Main from '@pages/Main/Animation';
 import Comments from '@pages/Comments/Animation';
 import Director from '@pages/Director/Animation';
 import Error from '@pages/Error/Error';
-import Header from '@components/layout/Header/Header';
-import Footer from '@components/layout/Footer/Footer';
-import GlobalStyle from '@styles/globalStyle';
-import theme from '@styles/theme';
-import Animation from '@pages/Animation';
 
 const router = createBrowserRouter([
-  { path: '/', element: <Main />, errorElement: <Error /> },
-  { path: '/main', element: <Main /> },
-  { path: '/comments', element: <Comments /> },
-  { path: '/director', element: <Director /> },
+  {
+    path: '/',
+    element: <MainLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Main /> },
+      { path: '/main', element: <Main /> },
+      { path: '/comments', element: <Comments /> },
+      { path: '/director', element: <Director /> },
+    ],
+  },
 ]);
 
 function App() {
@@ -22,11 +27,6 @@ function App() {
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
       <GlobalStyle />
-      <Header />
-      <main>
-        <Animation />
-      </main>
-      <Footer />
     </ThemeProvider>
   );
 }
