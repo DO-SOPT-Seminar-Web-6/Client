d2d183cf43d4c6063c9296b82612a4549fe048ad
Jeon Eonseok <jseokeon@gmail.com>
2023-11-23 16:50:11 +0900

    [ etc ] Router 파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {RouterProvider} from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@styles/globalStyle';
 import theme from '@styles/theme';
-
+import { router } from 'Router';
 
 function App() {
   return (
