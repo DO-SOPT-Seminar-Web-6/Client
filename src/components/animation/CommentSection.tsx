dc9e686a26906ccdea4e771bea83965c19bddbee
HAE2NI <hae0_02ni@ewhain.net>
2023-11-27 21:59:05 +0900

    [ etc ] 파일 폴도 별 분리

---
+++ src/components/animation/CommentSection.tsx
@@ -0,0 +1,11 @@
+import CommentTitle from './Comment/CommentTitle';
+import CommentInput from './Comment/CommentInput';
+
+export default function CommentSection() {
+  return (
+    <>
+      <CommentTitle />
+      <CommentInput />
+    </>
+  );
+}
