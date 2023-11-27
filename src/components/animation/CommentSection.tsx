08ceff5d83c331153aa4a129075dd233569d97c7
HAE2NI <hae0_02ni@ewhain.net>
2023-11-27 22:30:35 +0900

    [ design ] CommentSection

--- src/components/animation/CommentSection.tsx
+++ src/components/animation/CommentSection.tsx
@@ -1,11 +1,15 @@
 import CommentTitle from './Comment/CommentTitle';
 import CommentInput from './Comment/CommentInput';
+import CommentBox from './Comment/CommentBox';
+import { DivideLineButtom } from '@styles/common/divideLine';
 
 export default function CommentSection() {
   return (
     <>
       <CommentTitle />
       <CommentInput />
+      <DivideLineButtom />
+      <CommentBox />
     </>
   );
 }
