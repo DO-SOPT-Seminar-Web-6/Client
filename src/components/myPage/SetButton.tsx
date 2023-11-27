cbc7542cede4d0b28caee6a374d13e0b5dd120f5
Rose <120544840+Rose-my@users.noreply.github.com>
2023-11-27 20:11:34 +0900

    [ feat ] 모달의 상위 컴포넌트에서 props 전달

--- src/components/myPage/SetButton.tsx
+++ src/components/myPage/SetButton.tsx
@@ -1,53 +1,55 @@
-import styled from 'styled-components'
-import {BodyPlusNormalIc} from '@assets/index';
-import {BodyMenuHoverIc, BodyMenuNormalIc } from '@assets/index';
-import {BodyPencilHoverIc, BodyPencilNormalIc } from '@assets/index';
+import styled from 'styled-components';
+import { BodyPlusNormalIc, BodyPencilHoverIc, BodyPencilNormalIc, BodyMenuHoverIc, BodyMenuNormalIc } from '@assets/index';
 import { flexCenter } from '@styles/globalStyle';
 import { useState } from 'react';
 import { SetTitle } from '@styles/common/myPage/SetTitle';
 import { ButtonBox } from '@styles/Layout/MyPage/SetButtonBox';
+import MyPageModal from '@utils/Modal';
 
-export default function SetButton() {
-  const [isComment, setIsComment] = useState(false);
+interface InSetButtonPropsTypes{
+  comment: string;
+  setComment: React.Dispatch<React.SetStateAction<string>>;
+  isWriteComment: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-  function handleComment() {
-    setIsComment((prev) => !prev);
-  }
-
+export default function SetButton(props:InSetButtonPropsTypes) {
+  const {comment, setComment, isWriteComment} = props;
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [isSeeMore, setIsSeeMore] = useState(false);
 
-  function handleSeeMore(){
+  function handleOpenModal() {
+    setIsModalOpen(true);
+  }
+  function handleCloseModal(){
+    setIsModalOpen(false); 
+    isWriteComment(true);
+  }
+  function handleSeeMore() {
     setIsSeeMore((see) => !see);
   }
 
   return (
     <Container>
       <ButtonBox>
-        <BodyPlusNormalIc/>
+        <BodyPlusNormalIc />
         <SetTitle>보고싶어요</SetTitle>
       </ButtonBox>
-      <ButtonBox onClick={handleComment}>
-        {isComment && (
-          <BodyPencilHoverIc/>
-        )}
-        {!isComment &&(
-          <BodyPencilNormalIc/>
-        )}
+      <ButtonBox onClick={handleOpenModal}>
+        {isModalOpen && <BodyPencilHoverIc />} 
+        {!isModalOpen && <BodyPencilNormalIc />}
         <SetTitle>코멘트</SetTitle>
       </ButtonBox>
-      <ButtonBox onClick={handleSeeMore}>
-        {isSeeMore && (
-          <BodyMenuHoverIc/>
-        )}
-        {!isSeeMore &&(
-          <BodyMenuNormalIc/>
-        )}
+      <ButtonBox onClick={handleSeeMore}> 
+        {isSeeMore && <BodyMenuHoverIc />}
+        {!isSeeMore && <BodyMenuNormalIc />}
         <SetTitle>더보기</SetTitle>
       </ButtonBox>
+      {isModalOpen && <MyPageModal open={isModalOpen} close={handleCloseModal} comment={comment} setComment={setComment}/>}
     </Container>
-  )
+  );
 }
 
 const Container = styled.section`
   ${flexCenter};
-`
+  position: relative;
+`;
