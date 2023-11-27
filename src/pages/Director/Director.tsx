212a223848402250f629c2ceb4b745cd18b5b9ec
Jeon Eonseok <jseokeon@gmail.com>
2023-11-27 17:17:26 +0900

    [ feat/director ] 더보기 기능 구현

--- src/pages/Director/Director.tsx
+++ src/pages/Director/Director.tsx
@@ -2,9 +2,12 @@ import styled from 'styled-components';
 import CastDetailHayaoImg from '@assets/image/cast/castDetail/castDetailHayaoImg.png';
 import { CastDetailLikeIc } from '@assets/index';
 import { SectionContainer, flexCenter } from '@styles/globalStyle';
+import { useState } from 'react';
 
 /** director page */
 export default function Director() {
+  const [showText, setShowText] = useState(false);
+
   return (
     <>
       <Container>
@@ -15,7 +18,7 @@ export default function Director() {
         </div>
         <div>
           <SubTitle>바이오그래피</SubTitle>
-          <SubTitleDescription>
+          <SubTitleDescription showText={showText}>
             <SubTitleDescriptionText>
               1941년 도쿄에서 출생. 애니메이션 영화감독. 가쿠슈인(學習院)대학을 졸업한 후 1963년 도에이동화(東映動畵: 현
               도에이 애니메이션) 입사. 그 후 몇몇 창작회사를 거쳐 1985년 지브리 스튜디오(Studio Ghibli) 설립에 참가.
@@ -25,8 +28,14 @@ export default function Director() {
               돼지&gt;(紅の豚, 1992), &lt;원령공주&gt;(もののけ姬, 1997), &lt;센과 치히로의
               행방불명&gt;(千と千尋の神隱し,
             </SubTitleDescriptionText>
-            <SubTitleDescriptionHideBox>&nbsp;</SubTitleDescriptionHideBox>
-            <SubTitleDescriptionButton>더보기</SubTitleDescriptionButton>
+            {!showText && <SubTitleDescriptionHideBox>&nbsp;</SubTitleDescriptionHideBox>}
+            <SubTitleDescriptionButton
+              showText={showText}
+              onClick={() => {
+                setShowText(true);
+              }}>
+              더보기
+            </SubTitleDescriptionButton>
           </SubTitleDescription>
           <SubTitleRef>도서정보 및 바이오그래피 제공 | 알라딘</SubTitleRef>
         </div>
@@ -71,13 +80,17 @@ const SubTitle = styled.h1`
   color: ${({ theme }) => theme.colors.black};
 `;
 
-const SubTitleDescription = styled.div`
+interface InSubTitleDescriptionTypes {
+  showText: boolean;
+}
+
+const SubTitleDescription = styled.div<InSubTitleDescriptionTypes>`
   ${({ theme }) => theme.fonts.subTitle160};
 
   position: relative;
   margin-bottom: 1rem;
-  height: 7.8rem;
-  overflow: hidden;
+  height: ${({ showText }) => (showText ? 'auto' : '7.8rem')};
+  overflow: ${({ showText }) => (showText ? 'visible' : 'hidden')};
 `;
 
 const SubTitleDescriptionText = styled.p`
@@ -88,7 +101,6 @@ const SubTitleDescriptionHideBox = styled.div`
   position: absolute;
   right: 4.4rem;
   bottom: 0;
-  z-index: 1;
   background-color: ${({ theme }) => theme.colors.white};
   padding-left: 10rem;
   width: 4.5rem;
@@ -101,11 +113,11 @@ const SubTitleDescriptionHideBox = styled.div`
   -webkit-mask-position: left, right;
 `;
 
-const SubTitleDescriptionButton = styled.button`
+const SubTitleDescriptionButton = styled.button<InSubTitleDescriptionTypes>`
+  display: ${({ showText }) => (showText ? 'none' : 'block')};
   position: absolute;
   right: 0;
   bottom: 0;
-  z-index: 2;
   background-color: ${({ theme }) => theme.colors.white};
   color: ${({ theme }) => theme.colors.black};
 `;
