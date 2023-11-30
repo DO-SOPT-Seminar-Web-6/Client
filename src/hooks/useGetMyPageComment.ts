f9da5239866368e9640df6f795d9fbe9ce564377
Rose <120544840+Rose-my@users.noreply.github.com>
2023-11-30 22:41:53 +0900

    [ feat ] getMyPageComment에 사용하는 custom hook

---
+++ src/hooks/useGetMyPageComment.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "react-query";
+import { getMyPageComment, InGetMyPageCommentTypes } from "@api/getMyPageComment";
+import { CommentDataTypes } from "types/commentDataTypes";
+
+export default function useGetMyPageComment(){
+  const result=useQuery<CommentDataTypes, Error>
+  (['getMyPageComment'], getMyPageComment, {
+    onError: (error)=> {
+      console.error('에러 발생', error);
+    },
+  });
+
+  if(result.data){
+    console.log('데이터:', result.data);
+  }
+  return result;
+}
