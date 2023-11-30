5ab25623c9e7218dc22baaed03773ca5e760382f
HAE2NI <hae0_02ni@ewhain.net>
2023-11-30 23:14:01 +0900

    [ feat ] hover 시에 별이 채워지도록 수정

--- src/components/myPage/SetStars.tsx
+++ src/components/myPage/SetStars.tsx
@@ -1,24 +1,53 @@
-import { BodyStarFillIc } from '@assets/index';
+import { BodyStarEmptyIc, BodyStarFillIc, BodyStarHalfIc } from '@assets/index';
+import { flexCenter } from '@styles/globalStyle';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 export default function SetStars() {
+  let starRatingState: Array<boolean> = [false, false, false, false, false];
+  const [starRatingOnOff, setStarRatingOnOff] = useState(starRatingState);
+
+  function handleMouseOverStarRating(e: React.MouseEvent, idx: number) {
+    e.preventDefault();
+    let tempStarRating: Array<boolean> = [...starRatingOnOff];
+    for (let i = 0; i < 5; i++) {
+      if (i <= idx) {
+        tempStarRating[i] = true;
+      } else {
+        tempStarRating[i] = false;
+      }
+    }
+    setStarRatingOnOff(tempStarRating);
+  }
+
   return (
     <StarBox>
-      <BodyStarFillIcon />
-      <BodyStarFillIcon />
-      <BodyStarFillIcon />
-      <BodyStarFillIcon />
-      <BodyStarFillIcon />
+      {starRatingOnOff.map((isFilled, index) => (
+        <Star key={index} onMouseOver={(e) => handleMouseOverStarRating(e, index)}>
+          {isFilled ? <BodyStarFillIcon /> : <BodyStarEmptyIcon />}
+        </Star>
+      ))}
     </StarBox>
   );
 }
 
-const StarBox = styled.span`
+const StarBox = styled.div`
+  ${flexCenter}
   width: 27.5rem;
   height: 7rem;
 `;
 
+const Star = styled.div`
+  ${flexCenter}
+  cursor: pointer;
+`;
+
 const BodyStarFillIcon = styled(BodyStarFillIc)`
+  width: 4.5rem;
+  height: 4.5rem;
+`;
+
+const BodyStarEmptyIcon = styled(BodyStarEmptyIc)`
   width: 4.5rem;
   height: 4.5rem;
 `;
