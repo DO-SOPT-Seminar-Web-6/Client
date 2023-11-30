9d9678ebe0fa3b6a2e3d69fc27f5f040380006ec
Rose <120544840+Rose-my@users.noreply.github.com>
2023-12-01 03:33:07 +0900

    [ feat ] handleMoveToDirector

--- src/components/cast/Cast.tsx
+++ src/components/cast/Cast.tsx
@@ -3,25 +3,33 @@ import styled from 'styled-components';
 import CastHeader from './CastHeader';
 import CastArticle from './CastArticle';
 import Button from '@components/common/Button/Button';
+import { useNavigate } from 'react-router-dom';
+
 export default function Cast() {
+  const navigate = useNavigate();
+
+  function handleMoveToDirector() {
+    navigate('/director');
+  }
+
   return (
     <CastContainer>
-      <CastHeader/>
-      <CastDetailContainer>
-        <CastArticle/>
-        <Button/>
+      <CastHeader />
+      <CastDetailContainer onClick={handleMoveToDirector}>
+        <CastArticle />
+        <Button />
       </CastDetailContainer>
-    </CastContainer> 
-  )
-};
+    </CastContainer>
+  );
+}
 
 const CastContainer = styled(MainContainer)`
   gap: 3.5rem;
   padding: 5.5rem 6rem;
   height: 54.8rem;
-`
+`;
 const CastDetailContainer = styled(MainContainer)`
   gap: 3.5rem;
-  align-items:center;
-  height:36.8rem; 
-`
+  align-items: center;
+  height: 36.8rem;
+`;
