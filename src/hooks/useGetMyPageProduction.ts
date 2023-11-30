23123d57348e8919215cc85919c239d1f9613cc7
Rose <120544840+Rose-my@users.noreply.github.com>
2023-12-01 02:51:39 +0900

    [ fix ] useQuery 타입 수정

--- src/hooks/useGetMyPageProduction.ts
+++ src/hooks/useGetMyPageProduction.ts
@@ -1,16 +1,13 @@
 import { useQuery } from 'react-query';
-import { getMyPageProduction, InGetMyPageProductionTypes } from '@api/getMyPageProduction';
+import { getMyPageProduction} from '@api/getMyPageProduction';
+import { DataTypes } from 'types/dataTypes';
 
 export default function useGetMyPageProduction() {
-  const result = useQuery<InGetMyPageProductionTypes, Error>(['getMyPageProduction'], getMyPageProduction, {
+  const result = useQuery<DataTypes[], Error>(['getMyPageProduction'], getMyPageProduction, {
     onError: (error) => {
       console.log('에러 발생', error);
     },
   });
-  // Log the data when it is available
-  if (result.data) {
-    console.log('데이터:', result.data);
-  }
 
   return result;
 }
