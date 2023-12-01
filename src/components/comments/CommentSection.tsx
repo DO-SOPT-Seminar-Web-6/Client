fe62bbe3e52ed42000ae0bf7d865f0259e9222fb
Jeon Eonseok <jseokeon@gmail.com>
2023-12-01 12:52:00 +0900

    [ refactor ] useQuery를 이용하여 comments data 받아오기

--- src/components/comments/CommentSection.tsx
+++ src/components/comments/CommentSection.tsx
@@ -1,5 +1,7 @@
 import { customAxios } from '@api/customAxios';
+import getComment from '@api/getComment';
 import Comment from '@components/common/Comment/Comment';
+import useGetComment from '@hooks/useGetComment';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
@@ -9,22 +11,30 @@ interface InCommentSectionPropsTypes {
 
 //여기다가 나열해주시면 됩니다!
 export default function CommentSection({ sort }: InCommentSectionPropsTypes) {
-  const [data, setData] = useState([]);
+  // axios를 이용한 방법 -> 이건 됩니다.
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    async function todo() {
-      const response = await customAxios.get('/api/comment', {
-        params: {
-          sort,
-        },
-      });
-      setData(response.data.data);
-    }
-    todo();
-  }, [sort]);
+  // useEffect(() => {
+  //   async function todo() {
+  //     const response = await customAxios.get('/api/comment', {
+  //       params: {
+  //         sort,
+  //       },
+  //     });
+  //     setData(response.data.data);
+  //   }
+  //   todo();
+  // }, [sort]);
+
+  // useQuery를 이용한 방법 -> 아직 안 됨.
+  const { data, refetch } = useGetComment(sort);
+
+  console.log(sort);
 
   return (
     <Wrapper>
+      {/* 아래는 렌더링 되고 있는지 테스트를 위해 추가 */}
+      <h1>{sort}</h1>
       {data?.map(({ imageUrl, content, commentCount, isLike, likeCount, name, star }) => (
         <Comment
           key={`${imageUrl}-${content}`}
--- src/hooks/useGetMyPageComment.ts
+++ src/hooks/useGetMyPageComment.ts
@@ -1,11 +1,11 @@
-import { useQuery } from "react-query";
-import { getMyPageComment} from "@api/getMyPageComment";
-import { CommentDataTypes } from "types/commentDataTypes";
+import { useQuery } from 'react-query';
+import { getMyPageComment } from '@api/getMyPageComment';
+import { CommentDataTypes } from 'types/commentDataTypes';
 
-export default function useGetMyPageComment(){
-  const result=useQuery<CommentDataTypes[], Error>
-  (['getMyPageComment'], getMyPageComment, {
-    onError: (error)=> {
+export default function useGetMyPageComment() {
+  const result = useQuery<CommentDataTypes[], Error>(['getMyPageComment'], getMyPageComment, {
+    refetchOnMount: 'always',
+    onError: (error) => {
       console.error('에러 발생', error);
     },
   });
